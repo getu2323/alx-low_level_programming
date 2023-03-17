b3ad642f8f9eeee73ed75b1372e8d2ba57a8302b
getu2323 <getutegegne2323@gmail.com>
2023-03-16 17:38:59 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-  *main -> entry point
-  *return always 0 (success)
-  */
-int main(void)
+*main - entry point
+*return: always 0 (success)
+*/
+int main(void)/*this is main*/
 {
 	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
