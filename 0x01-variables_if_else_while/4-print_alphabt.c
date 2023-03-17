047b98b375f94970d9f0f622e20f6c713021d227
getu2323 <getutegegne2323@gmail.com>
2023-03-17 16:17:45 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char sl;
+	char cl;
+
+	for (sl = 'a'; sl <= 'z'; sl++)
+	{
+		if (sl != 'e' && sl != 'q')
+		{
+			putchar(sl);
+		}
+	}
+	putchar('\n');
+	/* your code goes there */
+	return (0);
+}
