d61e3d21b28bc2cda5a25c26802c48ab29130710
getu2323 <getutegegne2323@gmail.com>
2023-03-19 11:23:41 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-  *main - entry point
-  *return : always 0 (success)
-  */
+ * main - prints the text in quoutes.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
